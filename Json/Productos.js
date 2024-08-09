904ebe3f233d2ecfe4e6ac56140d5617d46ff4e7
AriMoreraC <ari208710961@gmail.com>
2024-08-08 18:44:46 -0600

    Update Productos.js
    10.5

--- Json/Productos.js
+++ Json/Productos.js
@@ -1,199 +1,211 @@
-const productos=[
+const productos = [
     {
         "id": 1,
-        "Nombre": "Prueba",
-        "imagen": "/Imagenes/InteriorEstudio.jpg",
+        "Nombre": "Producto 1",
+        "Descripcion": "Descripción del Producto 1",
+        "Imagen": "https://i1.sndcdn.com/artworks-000427032654-6q5sf7-t500x500.jpg",
         "Precio": 1000,
-        "Descripción": "",
-        "Galería": [
-            "./imagenes/Grafico.png",
-            "./imagenes/Grafico.png"
-        ],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "",
-        "Enlace a contacto": "",
-        "Garantía": "",
-        "Categorias": ["a", "b"]
+        "TiempoEntrega": "Inmediata",
+        "CostoEnvio": 1000,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "3 meses",
+        "Categorias": ["a", "c"],
+        "GaleriaImagenes": {
+            "Imagen1": "https://i1.sndcdn.com/artworks-000427032654-6q5sf7-t500x500.jpg",
+            "Imagen2": "https://i1.sndcdn.com/artworks-000427032654-6q5sf7-t500x500.jpg",
+            "Imagen3": "https://i1.sndcdn.com/artworks-000427032654-6q5sf7-t500x500.jpg"
+        }
     },
     {
         "id": 2,
-        "Nombre": "Prueba",
-        "imagen": "./imagenes/Grafico.png",
-        "Precio": 1500,
-        "Descripción": "a",
-        "Galería": [
-            "./imagenes/Grafico.png",
-            "./imagenes/Grafico.png"
-        ],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "",
-        "Enlace a contacto": "",
-        "Garantía": "",
-        "Categorias": ["c", "b"]
+        "Nombre": "Producto 2",
+        "Descripcion": "Descripción del Producto 2",
+        "Imagen": "https://goo.su/hXnf",
+        "Precio": 2000,
+        "TiempoEntrega": "Inmediata",
+        "CostoEnvio": 2000,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "2 meses",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "https://goo.su/hXnf",
+            "Imagen2": "https://goo.su/hXnf",
+            "Imagen3": "https://goo.su/hXnf"
+        }
     },
     {
         "id": 3,
-        "Nombre": "Prueba",
-        "imagen": "./imagenes/Grafico.png",
-        "Precio": 0,
-        "Descripción": "",
-        "Galería": [
-            "./imagenes/Grafico.png",
-            "./imagenes/Grafico.png"
-        ],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "",
-        "Enlace a contacto": "",
-        "Garantía": "",
-        "Categorias": ["", ""]
+        "Nombre": "Producto 3",
+        "Descripcion": "Descripción del Producto 3",
+        "Imagen": "https://i.pinimg.com/736x/cd/d8/5c/cdd85c55640e26468133175ddb381829.jpg",
+        "Precio": 3000,
+        "TiempoEntrega": "Inmediata",
+        "CostoEnvio": 3000,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "3 meses",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "https://i.pinimg.com/736x/cd/d8/5c/cdd85c55640e26468133175ddb381829.jpg",
+            "Imagen2": "https://i.pinimg.com/736x/cd/d8/5c/cdd85c55640e26468133175ddb381829.jpg",
+            "Imagen3": "https://i.pinimg.com/736x/cd/d8/5c/cdd85c55640e26468133175ddb381829.jpg"
+        }
     },
     {
         "id": 4,
-        "Nombre": "Prueba",
-        "imagen": "./imagenes/Grafico.png",
+        "Nombre": "Producto 4",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción": "",
-        "Galería": [
-            "./imagenes/Grafico.png",
-            "./imagenes/Grafico.png"
-        ],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "",
-        "Enlace a contacto": "",
-        "Garantía": "",
-        "Categorias": ["", ""]
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif",
+            "Imagen3": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":5,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 5,
+        "Nombre": "Producto 5",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]
-        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif",
+            "Imagen3": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":6,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 6,
+        "Nombre": "Producto 6",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]
-        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":7,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 7,
+        "Nombre": "Producto 7",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]
-        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":8,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 8,
+        "Nombre": "Producto 8",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]
-        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":9,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 9,
+        "Nombre": "Producto 9",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]
-        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":10,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 10,
+        "Nombre": "Producto 10",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]        
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":11,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 11,
+        "Nombre": "Producto 11",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""] 
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     },
     {
-        "id":12,
-        "Nombre":"Prueba" ,
-        "imagen":"./imagenes/Grafico.png",
+        "id": 12,
+        "Nombre": "Producto 12",
+        "Descripcion": "",
+        "Imagen": "./Imagenes/NoEncontrado.avif",
         "Precio": 0,
-        "Descripción":"",
-        "Galería" : ["./imagenes/Grafico.png","./imagenes/Grafico.png"],
-        "Tiempos de entrega": 0,
-        "Gastos de envío": 0,
-        "Opciones de entrega: ": "Envío postal y recogida en tienda",
-        "Enlace a información de cambios y devoluciones.": "" ,
-        "Enlace a contacto": "", 
-        "Garantía" :"",
-        "Categorias": ["", ""]            
+        "TiempoEntrega": "0 días",
+        "CostoEnvio": 0,
+        "Stock": "Disponible online y en tienda física",
+        "OpcionesEntrega": "Envío postal y recogida en tienda",
+        "Garantia": "",
+        "Categorias": ["", ""],
+        "GaleriaImagenes": {
+            "Imagen1": "./Imagenes/NoEncontrado.avif",
+            "Imagen2": "./Imagenes/NoEncontrado.avif"
+        }
     }
-]
+];
