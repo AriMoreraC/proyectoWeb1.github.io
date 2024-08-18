33a2c51420400a05d4e55620dfee02a5e9d1cdce
AriMoreraC <ari208710961@gmail.com>
2024-08-17 23:14:36 -0600

    Update Pago.js

--- Js/Pago.js
+++ Js/Pago.js
@@ -1,35 +1,34 @@
-
-  async function codigoBin(event){
+async function codigoBin(event) {
     event.preventDefault()
-    const codigo=document.getElementById("codigo").value
-    const apiURL=`https://data.handyapi.com/bin/${codigo}`
+    const codigo = document.getElementById("codigo").value
+    const apiURL = `https://data.handyapi.com/bin/${codigo}`
     try {
-        const response=await fetch(apiURL)
-        const data=await response.json()
-        if(data.Status=='NOT FOUND'){
-            document.getElementById("error").style.display="block"
-            document.getElementById("error").innerText=
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        if (data.Status == 'NOT FOUND') {
+            document.getElementById("error").style.display = "block"
+            document.getElementById("error").innerText =
                 "Código BIN no encontrado"
-            throw new 
-                Error("Error: "+response.status+" "+response.statusText)
+            throw new
+                Error("Error: " + response.status + " " + response.statusText)
             console.log(response)
-        }else{
-        console.log(data)
-        if (data.Scheme=='MASTERCARD') {
-          const img =document.querySelector("img")
-         img.src= "./Imagenes/mastercard.png"
         } else {
-          const img =document.querySelector("img")
-             img.src= "./Imagenes/visa.png"
-          
-        }
-    document.getElementById("tipoTarjeta").textContent=data.Scheme
-    document.getElementById("tipo").textContent=data.Type
-    document.getElementById("nivel").textContent=data.CardTier
-    document.getElementById("error").style.display="block"
-    document.getElementById("error").innerText=""
+            console.log(data)
+            if (data.Scheme == 'MASTERCARD') {
+                const img = document.querySelector("img")
+                img.src = "./Imagenes/mastercard.png"
+            } else {
+                const img = document.querySelector("img")
+                img.src = "./Imagenes/visa.png"
+
+            }
+            document.getElementById("tipoTarjeta").textContent = data.Scheme
+            document.getElementById("tipo").textContent = data.Type
+            document.getElementById("nivel").textContent = data.CardTier
+            document.getElementById("error").style.display = "block"
+            document.getElementById("error").innerText = ""
             let cartItem = {
-             medio: data.Scheme
+                medio: data.Scheme
             }
             if (localStorage.getItem('MedioPago')) {
                 localStorage.setItem('MedioPago', JSON.stringify(cartItem))
@@ -37,11 +36,13 @@
                 console.log(JSON.parse(localStorage.getItem('MedioPago')))
                 $.notify("Compra Realizada con exitó", "warn");
             }
+
+
         }
-      
+
 
     } catch (error) {
-        console.log("Error: ",error)
+        console.log("Error: ", error)
     }
-    
+
 } 
