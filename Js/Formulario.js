109a637b468912a51744b87260e1e383e6ddc6c4
SebastianTV26 <torrellas155@gmail.com>
2024-08-17 18:33:18 -0600

    Update

--- Js/Formulario.js
+++ Js/Formulario.js
@@ -5,7 +5,6 @@ document.getElementById('provincia').addEventListener('change', function() {
         document.getElementById('error-provincia').style.display = 'none';
     }
 });
-
 document.querySelector('form').addEventListener('submit', function(event) {
     var provinciaSeleccionada = document.getElementById('provincia').value;
     if (provinciaSeleccionada === "Elija su provincia") {
@@ -18,20 +17,30 @@ document.querySelector('form').addEventListener('submit', function(event) {
 document.getElementById('fechaNacimiento').addEventListener('change', function() {
     var fecha = this.value;
     var fechaNacimiento = new Date(fecha);
-    var fechaMinima = new Date('2012-01-01'); // Fecha mínima para que tenga al menos 12 años
-
+    var fechaMaxima = new Date('1920-01-01');
+    var fechaMinima = new Date('2012-01-01');
     if (fechaNacimiento < fechaMinima) {
         document.getElementById('error-edadMenor').style.display = 'none';
+    } else {
+        document.getElementById('error-edadMenor').style.display = 'block';
+    }
+    if (fechaNacimiento >= fechaMaxima) {
+        document.getElementById('error-edadMayor').style.display = 'none';
+    } else {
+        document.getElementById('error-edadMayor').style.display = 'block';
     }
 });
-
 document.querySelector('form').addEventListener('submit', function(event) {
     var fecha = document.getElementById('fechaNacimiento').value;
     var fechaNacimiento = new Date(fecha);
-    var fechaMinima = new Date('2012-01-01'); // Fecha mínima para que tenga al menos 12 años
-
-    if (fechaNacimiento >= fechaMinima) {
+    var fechaMaxima = new Date('1920-01-01');
+    var fechaMinima = new Date('2012-01-01');
+    if (fechaNacimiento >= fechaMinima || fechaNacimiento < fechaMaxima) { // Que tenga al menos 12 años
         event.preventDefault(); // Evita el envío del formulario
         document.getElementById('error-edadMenor').style.display = 'block';
+    }
+    if (fechaNacimiento < fechaMaxima || fechaNacimiento >= fechaMinima) { // Máximo 104 años
+        event.preventDefault(); // Evita el envío del formulario
+        document.getElementById('error-edadMayor').style.display = 'block';
     }
 });
