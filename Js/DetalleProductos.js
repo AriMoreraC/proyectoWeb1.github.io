ca19f50d88b5e20a81c1bc4a0fa441e65e15eb6f
SebastianTV26 <torrellas155@gmail.com>
2024-08-20 18:31:24 -0600

    Update

--- Js/DetalleProductos.js
+++ Js/DetalleProductos.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             $("#Descripcion").html("<b>Descripción: </b>" + producto.Descripcion);
             $("#Precio").html("<b>Precio: </b>₡ " + producto.Precio);
             $("#Imagen").attr("src", producto.Imagen ? producto.Imagen : './Imagenes/NoEncontrado.avif');
-            $("#TiempoEntrega").html("<b>Tiempo de entrega: </b>₡ " + producto.TiempoEntrega);
+            $("#TiempoEntrega").html("<b>Tiempo de entrega: </b>" + producto.TiempoEntrega);
             $("#CostoEnvio").html("<b>Costo del envío: </b>₡ " + producto.CostoEnvio);
             $("#Stock").html("<b>Stock: </b>" + producto.Stock);
             $("#OpcionesEntrega").html("<b>Opciones de entrega: </b>" + producto.OpcionesEntrega);
@@ -17,12 +17,8 @@ $(document).ready(function () {
             $("#Imagen1").attr("src", producto.GaleriaImagenes.Imagen1)
             $("#Imagen2").attr("src", producto.GaleriaImagenes.Imagen2)
             $("#Imagen3").attr("src", producto.GaleriaImagenes.Imagen3)
-            // if (producto.Categorias && producto.Categorias.length > 0) {
             const categoriasHtml = producto.Categorias.filter(categoria => categoria).join(", ");
             $("#Categorias").html("<b>Categorías: </b>" + categoriasHtml);
-            // }
-            // Asignar el id del producto al botón
-            $("#btnAgregarAlCarrito").attr("data-id", producto.id);
         } else {
           window.location.href = `ProductosServicios.html`;
           alert("Producto no encontrado.");
@@ -31,34 +27,4 @@ $(document).ready(function () {
       window.location.href = `ProductosServicios.html`;
       alert("Producto no especificado.");
     }
-  
-    // Función para añadir al carrito en el botón
-    // $("#btnAgregarAlCarrito").on("click", function () {
-    //     let id = $(this).attr("data-id");
-    //     addToCart(id);
-    // });
   });
-  
-// Añadir al carrito
-// function addToCart(id) {
-//     const producto = productos.find((p) => p.id == id);
-//     if (!producto) return;
-//     let cartItem = {
-//         id: producto.id,
-//         nombre: producto.Nombre,
-//         precio: parseFloat(producto.Precio),
-//         cantidad: 1,
-//         costo: parseFloat(producto.CostoEnvio),
-//         subTotal: parseFloat(producto.Precio)
-//     };
-//     let cartArray = JSON.parse(localStorage.getItem('compra')) || [];
-//     let seleccion = cartArray.findIndex((prod) => prod.id == id);
-//     if (seleccion != -1) {
-//         cartArray[seleccion].cantidad += 1;
-//         cartArray[seleccion].subTotal = cartArray[seleccion].precio * cartArray[seleccion].cantidad;
-//     } else {
-//         cartArray.push(cartItem);
-//     }
-//     localStorage.setItem('compra', JSON.stringify(cartArray));
-//     $.notify("Producto o Servicio agregado: " + producto.Nombre, "success");
-//   }
